9a0bf3a56d515ee650067e970608309ee84d1633
Lucas Lucena <lucas.lucenak@gmail.com>
2022-12-23 11:48:05 -0300

    Add components

---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,16 @@
+@props(['listing'])
+<x-card>
+    <div class="flex">
+        <img class="hidden w-48 mr-6 md:block" src="{{asset('img/no-image.png') }}" alt=""/>
+        <div>
+            <h3 class="text-2xl">
+                <a href="listing/{{ $listing->id }}">{{ $listing->title }}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{ $listing->company_name }}</div>
+                <x-listing-tags :tags="$listing->tags"/>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$listing->description}}
+            </div>
+        </div>
+    </div>
+</x-card>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -15,29 +15,7 @@
     <div class="lg:grid lg:gird-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
         @foreach ($listings as  $listing)
-        <div class="bg-gray-50 border border-gray-200 rounded p-6">
-            <div class="flex">
-                <img class="hidden w-48 mr-6 md:block" src="{{asset('img/no-image.png') }}" alt=""/>
-                <div>
-                    <h3 class="text-2xl">
-                        <a href="listing/{{ $listing->id }}">{{ $listing->title }}</a>
-                    </h3>
-                    <div class="text-xl font-bold mb-4">{{ $listing->company_name }}</div>
-                    <ul class="flex">
-
-                        @foreach (explode(",", $listing->tags) as  $tag)
-                        <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                            <a href="#">{{ $tag }}</a>
-                        </li> 
-                        @endforeach
-
-                    </ul>
-                    <div class="text-lg mt-4">
-                        <i class="fa-solid fa-location-dot"></i> {{$listing->description}}
-                    </div>
-                </div>
-            </div>
-        </div>
+            <x-listing-card :listing="$listing"/>
         @endforeach
     </div>
     @endsection
